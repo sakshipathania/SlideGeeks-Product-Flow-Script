68f50eded84e0ea050a801b6cdd4d29a88a9587a
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-03-03 09:29:43 +0530

    Update Email_Sign_In_Incorrect_Data_6.java

--- src/test/java/SetupClass/StepDefinition/Email_Sign_In_Incorrect_Data_6.java
+++ src/test/java/SetupClass/StepDefinition/Email_Sign_In_Incorrect_Data_6.java
@@ -29,9 +29,9 @@ public class Email_Sign_In_Incorrect_Data_6 extends SetupClass {
 	@Then("^user navigates to login page vi$")
 	public void user_navigates_to_login_page_vi() throws Throwable {
 		
-		WebElement login_signup_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".t3-navbar > ul:nth-child(1) > li:nth-child(10) > a:nth-child(1)")));
+		WebElement login_signin_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[1]/a")));
 		 Thread.sleep(3000);
-		 login_signup_btn.click();
+		 login_signin_btn.click();
 		 Thread.sleep(3000);
 	    
 	}
@@ -39,18 +39,18 @@ public class Email_Sign_In_Incorrect_Data_6 extends SetupClass {
 	@Then("^user login to website with invalid email and password vi$")
 	public void user_login_to_website_with_invalid_email_and_password_vi() throws Throwable {
 		
-		WebElement login_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='pg-account-action']/div/div[2]/div/div[2]/form/div[1]/span/input")));
+		WebElement login_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div/form/div[1]/input")));
 		 Thread.sleep(3000);
 		js.executeScript("arguments[0].scrollIntoView();",login_email);
 		 login_email.sendKeys("hkjfujsid@hsujrf.hjsdgf");
 		 Thread.sleep(3000);
 		 
-		 WebElement login_password = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span.pg-login-form-password > input:nth-child(1)")));
+		WebElement login_password = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div/form/div[2]/input")));
 		 Thread.sleep(3000);
 		login_password.sendKeys("12#@#@#3456");
 		 Thread.sleep(3000);
 		 
-		 WebElement login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".pg-button-login")));
+		 WebElement login_btn=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-account-action > div > div:nth-child(2) > div > form > div.btn-wrapper.login_btn > input")));
 		 Thread.sleep(3000);
 		 login_btn.click();
 		 Thread.sleep(3000);
@@ -65,5 +65,27 @@ public class Email_Sign_In_Incorrect_Data_6 extends SetupClass {
 		
 	}
 
+	@Then("^user login to website with valid email and password vi$")
+	public void user_login_to_website_with_valid_email_and_password_vi() throws Throwable {
+		
+		Thread.sleep(3000);
+		WebElement old_paid_email1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div/form/div[1]/input")));
+               old_paid_email1.sendKeys("sakshi.pathania@slidetech.in");
+          Thread.sleep(3000);
+               WebElement old_paid_pass1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div/form/div[2]/input")));
+               old_paid_pass1.sendKeys("Qwerty@1");
+	       
+	       Thread.sleep(3000);
+	       WebElement old_paid_login_btn1=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-account-action > div > div:nth-child(2) > div > form > div.btn-wrapper.login_btn > input")));
+	       old_paid_login_btn1.click();
+		
+		driver.get("https://www.slidegeeks.com/business/product/up-skilling-process-vector-with-progress-chart-ppt-powerpoint-presentation-layouts-summary-pdf");
+		Thread.sleep(5000);
+		 
+		 WebElement download_btn1 = driver.findElement(By.xpath("//*[@class=\"btn-download pg-button pg-addtocart pg-green-background-btn\"]"));
+		 Thread.sleep(3000);
+		 download_btn1.click();
+		 Thread.sleep(3000);
+	}
 
 }
