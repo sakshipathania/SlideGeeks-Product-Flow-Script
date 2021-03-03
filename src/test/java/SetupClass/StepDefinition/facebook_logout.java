658b8b4ed08a6ffdae85f03df79648befc62284e
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-03-03 16:59:50 +0530

    Update facebook_logout.java

--- src/test/java/SetupClass/StepDefinition/facebook_logout.java
+++ src/test/java/SetupClass/StepDefinition/facebook_logout.java
@@ -18,11 +18,11 @@ public void navigate_to_the_facebook_url() throws InterruptedException {
 	Thread.sleep(2000);
 	driver.get("https://www.facebook.com");
     Thread.sleep(7000);
-	webelement account = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/i"));
+	WebElement account = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/i"));
 	Thread.sleep(5000);
 	account.click();
 	Thread.sleep(7000);
-	webelement logout = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div[3]/div/div[4]/div/div[1]/div[2]/div/div/div/div/span"));
+	WebElement logout = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div[3]/div/div[4]/div/div[1]/div[2]/div/div/div/div/span"));
 	Thread.sleep(5000);
 	logout.click();
 }
