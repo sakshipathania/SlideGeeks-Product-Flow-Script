c86e9e09e6ddf81e24774d3210a04de6bae2d786
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-03-04 11:10:19 +0530

    Update Google_logout.java

--- src/test/java/SetupClass/StepDefinition/Google_logout.java
+++ src/test/java/SetupClass/StepDefinition/Google_logout.java
@@ -1,11 +1,17 @@
-package SetupClass.TestStep;
+package SetupClass.StepDefinition;
 
-import SetupClass.Set;
+import SetupClass.SetupClass;
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+	
 
 public class Google_logout extends Set {
 
-
+WebDriverWait wait = new WebDriverWait(driver,50); 
+	
 @Given("^open the google plus page\\.$")
 public void open_the_google_plus_page() throws InterruptedException {
     Thread.sleep(3000);
