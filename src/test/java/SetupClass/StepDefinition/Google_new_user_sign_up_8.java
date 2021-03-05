a86efe6da84a85b66605f9c5334129a2997d417a
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-03-05 09:51:30 +0530

    Update Google_new_user_sign_up_8.java

--- src/test/java/SetupClass/StepDefinition/Google_new_user_sign_up_8.java
+++ src/test/java/SetupClass/StepDefinition/Google_new_user_sign_up_8.java
@@ -98,6 +98,12 @@ public class Google_new_user_sign_up_8 extends SetupClass {
 		js.executeScript("arguments[0].scrollIntoView();",Download);
 		Download.click();
 		Thread.sleep(3000);
+		 WebElement download_btn = driver.findElement(By.xpath("//*[@class=\"btn-download pg-button pg-addtocart pg-green-background-btn\"]"));
+		 Thread.sleep(3000);
+		js.executeScript("arguments[0].scrollIntoView();",download_btn);
+		Thread.sleep(3000);
+		 download_btn.click();
+		 Thread.sleep(3000);
 		 
 	    	}
 
