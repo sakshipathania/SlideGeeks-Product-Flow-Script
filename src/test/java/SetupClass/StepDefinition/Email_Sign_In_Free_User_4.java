9a1c7ce151ec38e0f2b021b404ad719314624c80
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-05-28 15:20:37 +0530

    Update Email_Sign_In_Free_User_4.java

--- src/test/java/SetupClass/StepDefinition/Email_Sign_In_Free_User_4.java
+++ src/test/java/SetupClass/StepDefinition/Email_Sign_In_Free_User_4.java
@@ -15,18 +15,26 @@ public class Email_Sign_In_Free_User_4 extends SetupClass {
 	
 	WebDriverWait wait = new WebDriverWait(driver,30);
         JavascriptExecutor js = (JavascriptExecutor) driver;
+	
 	@Given("^user is already on Home Page of Geeks Website iv$")
 	public void user_is_already_on_Home_Page_of_Geeks_Website_iv() throws Throwable {
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		log.info("It's opening the website URL");
 		Thread.sleep(1000);
+		
+		driver.get("https://www.slidegeeks.com/business/product/skills-free-powerpoint-slide");
+		Thread.sleep(3000);
+		 WebElement download_btn = driver.findElement(By.xpath("//*[@class=\"btn-download pg-button pg-addtocart pg-green-background-btn\"]"));
+		 Thread.sleep(3000);
+		 download_btn.click();
+		 Thread.sleep(3000);
 	}
 
 	@Then("^user navigates to login page iv$")
 	public void user_navigates_to_login_page_iv() throws Throwable {
-		WebElement login_signin_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[1]/a")));
-		 Thread.sleep(3000);
-		 login_signin_btn.click();
+		//WebElement login_signin_btn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/header/div/div/nav/div/div[2]/div[2]/div[2]/div/div[2]/ul/li[1]/a")));
+		// Thread.sleep(3000);
+		// login_signin_btn.click();
 		 Thread.sleep(3000);
 	}
 
@@ -47,9 +55,13 @@ public class Email_Sign_In_Free_User_4 extends SetupClass {
 	       old_paid_login_btn.click();
 	}
 
-	@Then("^user is redirected to Pricing page iv$")
-	public void user_is_redirected_to_Pricing_page_iv() throws Throwable {
-		log.info("Hey, I am on Pricing Page");
+	@Then("^user is redirected to Product page iv$")
+	public void user_is_redirected_to_Product_page_iv() throws Throwable {
+		//log.info("Hey, I am on Pricing Page");
+		 Thread.sleep(3000);
+		 WebElement download_btn = driver.findElement(By.xpath("//*[@class=\"btn-download pg-button pg-addtocart pg-green-background-btn\"]"));
+		 Thread.sleep(3000);
+		 download_btn.click();
 		 Thread.sleep(3000);
 	}
 
